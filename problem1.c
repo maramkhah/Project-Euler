cc63e987a1a4501bc48eccb886ab1d0bf388d73f
Shayon Maramkhah <frost00@github.com>
2024-05-07 17:49:29 -0400

    Adding problem 1

---
+++ problem1.c
@@ -0,0 +1,33 @@
+/****************************************************
+ * Programmer: Maramkhah, Shayon
+ * 
+ * Description: Find the sum of multiples 3 or 5 under 1000
+ * 
+ * 
+ * 
+*/
+#include <stdio.h>
+
+int main(void){
+    unsigned int limit = 1000;
+    const unsigned int M = 3;
+    const unsigned int commonDifference= M;
+    unsigned int n = (limit-1)/M;
+    unsigned int an = commonDifference+(n-1)*commonDifference;
+    unsigned int Sn= n*(M+an)/2;
+
+    unsigned int nn= (limit -1)/5;
+    unsigned int W = 5;
+    unsigned int commondiff= W;
+    unsigned int ann = commondiff+(nn-1)*commondiff;
+    unsigned int Sm= nn*(W+ann)/2;
+
+    unsigned int Z =15;
+    unsigned int nnn= (limit-1)/Z;
+    unsigned int comdif=Z;
+    unsigned int annn= comdif+(nnn-1)*comdif;
+    unsigned int Sz= nnn*(Z+annn)/2;
+
+    unsigned int sum = Sn+Sm-Sz;
+    printf("%u",sum);
+}
